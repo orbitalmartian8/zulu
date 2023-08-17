e6f166da5ffc190cf9eb2c43945b335ccaad4e05
CJ <83363242+orbitalmartian8@users.noreply.github.com>
2023-08-17 21:53:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-function updateClock() {
+function updateDigitalClock() {
   const now = new Date();
-  const hours = now.getUTCHours();
-  const minutes = now.getUTCMinutes();
-  const seconds = now.getUTCSeconds();
+  const hours = now.getUTCHours().toString().padStart(2, '0');
+  const minutes = now.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = now.getUTCSeconds().toString().padStart(2, '0');
 
-  const hourHand = document.querySelector('.hour');
-  const minuteHand = document.querySelector('.minute');
-  const secondHand = document.querySelector('.second');
+  const hoursElement = document.getElementById('hours');
+  const minutesElement = document.getElementById('minutes');
+  const secondsElement = document.getElementById('seconds');
 
-  const hourDeg = (hours % 12) * 30 + minutes * 0.5; // 30 degrees per hour, 0.5 degrees per minute
-  const minuteDeg = minutes * 6; // 6 degrees per minute
-  const secondDeg = seconds * 6; // 6 degrees per second
-
-  hourHand.style.transform = `rotate(${hourDeg}deg)`;
-  minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
-  secondHand.style.transform = `rotate(${secondDeg}deg)`;
+  hoursElement.textContent = hours;
+  minutesElement.textContent = minutes;
+  secondsElement.textContent = seconds;
 }
 
-updateClock();
-setInterval(updateClock, 1000);
+updateDigitalClock();
+setInterval(updateDigitalClock, 1000);
